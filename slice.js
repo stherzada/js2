8b62f8a6cc0b94332f630a7b9c07d9111fcb81a3
Sther <sthefanypequeno@gmail.com>
2023-03-30 15:01:05 -0300

    Módulo 1

---
+++ slice.js
@@ -0,0 +1,7 @@
+const nomes = ["Ana", "Bruno", "Carla", "Diego", "Eduarda", "Felipe", "Gabriela", "Henrique", "Isabela", "João"];
+
+const sala1 = nomes.slice(0,nomes.length/2)
+const sala2 = nomes.slice(nomes.length/2)
+
+console.log(`Alunos da sala 1: ${sala1}`)
+console.log(`Alunos da sala 1: ${sala2}`)
---
+++ splice.js
@@ -0,0 +1,9 @@
+
+
+const listaDeChamada = ["Alice", "Breno", "Cristina", "David", "Elena", "Fabiano"];
+
+
+// listaDeChamada.splice(1,2,'Rodrigo')
+
+listaDeChamada.splice(2,0, 'Rodrigo')
+console.log(`Lista de chamada ${listaDeChamada}`)
